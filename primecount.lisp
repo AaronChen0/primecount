1ba261a99bb53d3f021e239fb72882b58a702c12
AaronChen0 <aaronchen.lisp@gmail.com>
2020-02-16 13:42:19 +0800

    Update primecount.lisp

--- primecount.lisp
+++ primecount.lisp
@@ -1,7 +1,9 @@
+;;;* Author: Aaron Chen
+;;;* Last modified: 2020-02-16
+
 (in-package :primecount)
 
 (deftype u64 () '(unsigned-byte 64))
-(deftype u32 () '(unsigned-byte 32))
 (deftype u128 () '(unsigned-byte 128))
 
 (defun prime-count (n)
@@ -10,25 +12,24 @@
   (let* ((sq (isqrt n))
          (pis (make-array (+ sq 1) :element-type 'u64))
          (pil (make-array (+ sq 1) :element-type 'u64)))
-    (declare (type u32 sq)
+    (declare (type u64 sq)
              (type (simple-array u64) pis pil))
     (setf (aref pis 0) 0 (aref pil 0) 0)
-    (loop for i of-type u32 from 1 to sq
-       do (setf (aref pis i) (- i 1)))
-    (loop for i of-type u32 from 1 to sq
-       do (setf (aref pil i) (- (floor n i) 1)))
-    (loop for p of-type u32 from 2 to sq
+    (loop for i of-type u64 from 1 to sq
+       do (setf (aref pis i) (- i 1)
+                (aref pil i) (- (floor n i) 1)))
+    (loop for p of-type u64 from 2 to sq
        if (/= (aref pis p) (aref pis (- p 1)))
        do (let ((pc (aref pis (- p 1))) (p2 (* p p)))
             (declare (type u64 pc p2))
-            (loop for i of-type u32 from 1 to (min sq (floor n p2))
+            (loop for i of-type u64 from 1 to (min sq (floor n p2))
                for d of-type u64 = (* i p)
                if (<= d sq)
                do (decf (aref pil i) (the u64 (- (aref pil d) pc)))
                else
                do (decf (aref pil i)
                         (the u64 (- (aref pis (floor n d)) pc))))
-            (loop for i of-type u32 downfrom sq to p2
+            (loop for i of-type u64 downfrom sq to p2
                do (decf (aref pis i)
                         (the u64 (- (aref pis (floor i p)) pc))))))
     (the fixnum (aref pil 1))))
@@ -63,13 +64,13 @@
                  do (decf (aref s i)
                           (* p
                              (- (aref s (check (floor (aref v i) p) n ndr size))
-                                  sp))))))
+                                sp))))))
       (aref s 0))))
 
 (defun prime-count-mod4 (n)
   "Count primes of the forms 4m+1 and 4m+3 no larger than n."
   (declare (optimize (speed 3) (safety 0))
-           (type fixnum n))
+           (type u64 n))
   (let* ((r (isqrt n))
          (ndr (floor n r))
          (size (- (the u64 (+ r ndr)) 1))
@@ -79,8 +80,8 @@
     (declare (type u64 r ndr size)
              (type (simple-array u64) v s1 s3))
     (flet ((check (v)
-               (declare (type u64 v))
-               (if (< v ndr) (- size v) (- (floor n v) 1))))
+             (declare (type u64 v))
+             (if (< v ndr) (- size v) (the u64 (- (floor n v) 1)))))
       (declare (inline check))
       (loop for i below r do (setf (aref v i) (floor n (+ i 1))))
       (loop for i from r
@@ -97,7 +98,7 @@
                        (p2 (* p p)))
                    (declare (type u64 sp sp3 p2))
                    (loop for i below size
-                      for tmp = (check (floor (aref v i) p))
+                      for tmp of-type u64 = (check (floor (aref v i) p))
                       until (< (aref v i) p2) do
                         (decf (aref s1 i)
                               (the u64 (- (aref s1 tmp) sp)))
@@ -109,7 +110,7 @@
                        (p2 (* p p)))
                    (declare (type u64 sp sp3 p2))
                    (loop for i below size
-                      for tmp = (check (floor (aref v i) p))
+                      for tmp of-type u64 = (check (floor (aref v i) p))
                       until (< (aref v i) p2) do
                         (decf (aref s1 i)
                               (the u64 (- (aref s3 tmp) sp3)))
